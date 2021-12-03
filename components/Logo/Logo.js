9ec4e5874b1f8d89c32f3f8851124007bd8b2208
Timur <jesorstaff@gmail.com>
2021-12-03 14:19:07 +0500

    styles & mobile menu

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,17 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./Logo.module.css"
+import styles from "./Logo.module.css";
 import logoImage from "../../public/logo.png";
 
 export default function Logo() {
   return (
     <Link href="/">
-      <a className={styles.logo} style={{ marginRight: '40px' }}>
+      <a className={styles.logo}>
         <Image src={logoImage} alt={"Logo"} width={20} height={20} />
         <p className={styles.logoStyle}>Timur Bayramgulov</p>
       </a>
     </Link>
   );
 }
-
-
--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,28 +1,123 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from "../Logo/Logo";
-import SwitchTheme from "../SwitchTheme/SwitchTheme"
-import styles from "./Nav.module.css"
+import SwitchTheme from "../SwitchTheme/SwitchTheme";
+import styles from "./Nav.module.css";
 
 export default function Nav() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth < 620) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
+
+  const MenuDesktop = () => {
+    return (
+      <div className={styles.menu}>
+        <Link href="/Works">
+          <a>Works</a>
+        </Link>
+        <Link href="https://github.com/jesorstaff">
+          <a>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 512 512"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="black"
+                d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"
+              />
+            </svg>
+            <span>Source</span>
+          </a>
+        </Link>
+      </div>
+    );
+  };
+
+  const MenuMobile = () => {
+    const [active, setActive] = useState(false);
+
+    return (
+      <>
+        <button
+          className={styles.open_menu_button}
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          <svg
+            viewBox="0 0 24 24"
+            focusable="false"
+            aria-hidden="true"
+            width="16px"
+            height="16px"
+          >
+            <path
+              fill="currentColor"
+              d="M 3 5 A 1.0001 1.0001 0 1 0 3 7 L 21 7 A 1.0001 1.0001 0 1 0 21 5 L 3 5 z M 3 11 A 1.0001 1.0001 0 1 0 3 13 L 21 13 A 1.0001 1.0001 0 1 0 21 11 L 3 11 z M 3 17 A 1.0001 1.0001 0 1 0 3 19 L 21 19 A 1.0001 1.0001 0 1 0 21 17 L 3 17 z"
+            ></path>
+          </svg>
+        </button>
+        <div className={`${styles.popup} ${active ? styles.open : null}`}>
+          <Link href="/Works">
+            <a>Works</a>
+          </Link>
+          <Link href="https://github.com/jesorstaff">
+            <a>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 512 512"
+                height="16"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="black"
+                  d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"
+                />
+              </svg>
+              <span>Source</span>
+            </a>
+          </Link>
+        </div>
+      </>
+    );
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.navigation}>
         <div className={styles.row}>
           <Logo />
-          <div className={styles.menu}>
-            <Link href="/Works"><a>Works</a></Link>
-              <Link href="https://github.com/jesorstaff">
-                <a>
-                  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                    <path fill="black" d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z" />
-                  </svg>
-                  <span>Source</span>
-                </a>
-              </Link>
-            </div>
+          {isMobile ? null : <MenuDesktop />}
         </div>
-        <SwitchTheme />
+        {isMobile ? (
+          <div className={styles.for_mobile}>
+            <SwitchTheme />
+            <MenuMobile />
+          </div>
+        ) : (
+          <SwitchTheme />
+        )}
       </div>
     </header>
   );
-};
+}
